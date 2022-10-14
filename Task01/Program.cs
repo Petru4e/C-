af95fad14f0617b559426924b11e8f0106315896
Petru4e <Petru4e85@mail.ru>
2022-10-14 12:41:36 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.Write("Enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine((number2 == number1/number2)?"yes":"no");
