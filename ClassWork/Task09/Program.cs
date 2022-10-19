198d0e37a8fced5d922b15bd85197a9ce4876b87
Petru4e <Petru4e85@mail.ru>
2022-10-19 09:28:31 +0300

    Task09

---
+++ ClassWork/Task09/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(10,100);
+System.Console.WriteLine($"Сучайное число из диапазона 10-99-> {num}");
+
+int firstDigit = num/10;
+int secondDigit = num%10;
+int max = firstDigit>secondDigit?firstDigit:secondDigit;
+System.Console.WriteLine($" Наибольшая цифра числа {num} равна {max}");
