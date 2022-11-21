622b013d4a54ae8eab1fe332135174b80eec945e
Petru4e <Petru4e85@mail.ru>
2022-11-21 14:33:17 +0300

    Task50 with tuple

--- HomeWork/Task50/Program.cs
+++ HomeWork/Task50/Program.cs
@@ -27,9 +27,34 @@ PrintArray(array2d);
 Console.WriteLine();
 SearchElem(array2d.GetLength(0), array2d.GetLength(1));
 
+//проба пера с кортежами
+#region  Tuple
+Console.WriteLine("Solution #1 via tuple");
+var check = Check(m, n, array2d.GetLength(0), array2d.GetLength(1));
+Console.WriteLine(check.row && check.column ? $"Element [{m}, {n}] value {array2d[m, n]}." : "There is no such element in the array");
+
+Console.WriteLine("Solution #2 via tuple");
+var checkArray = CheckArray(m, n, array2d.GetLength(0), array2d.GetLength(1));
+Console.WriteLine(checkArray.row >= 0 && checkArray.column >= 0 ? $"Element [{checkArray.row}, {checkArray.column}] value {array2d[checkArray.row,checkArray.column]}." : "There is no such element in the array");
+(int row, int column) CheckArray(int myRow, int myColumn, int arrayRow, int arrayColumn)
+{
+    var result = (row: -1, column: -1);
+    if (myRow < arrayRow) result.row = myRow;
+    if (myColumn < arrayColumn) result.column = myColumn;
+    return result;
+}
+
+(bool row, bool column) Check(int myRow, int myColumn, int arrayRow, int arrayColumn)
+{
+    return ((myRow < arrayRow), (myColumn < arrayColumn));
+}
+
+#endregion
+
+
 void SearchElem(int row, int column)
 {
-    if (m < row && n < column) Console.WriteLine($"Element [{m}, {n}] value {array2d[m,n]}.");
+    if (m < row && n < column) Console.WriteLine($"Element [{m}, {n}] value {array2d[m, n]}.");
     else Console.WriteLine("There is no such element in the array");
 }
 
